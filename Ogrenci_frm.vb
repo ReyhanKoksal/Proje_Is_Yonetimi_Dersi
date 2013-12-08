338b76ebf880242758f202e2d6fc58e6be56f202
Reyhan Köksal <r3yh4n@hotmail.com>
2013-12-08 13:53:06 +0200

    Anaform - ogr&velibuton_click
    veli buton_click tamamlandı

--- Ogrenci_frm.vb
+++ Ogrenci_frm.vb
@@ -122,3 +122,117 @@ Private Sub veli_ekle_btn_Click(ByVal sender As System.Object, ByVal e As System
             veli_soyad_txt.Focus()
             veli_soyad_txt.Text = ""
 
+
+        ElseIf IsNumeric(tc_txt.Text) = False Then
+            MsgBox("Lütfen TC No Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            tc_txt.Focus()
+            tc_txt.Text = ""
+
+
+        ElseIf IsNumeric(veli_ad_txt.Text) = True Then
+            MsgBox("Lütfen Veli Ad Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            veli_ad_txt.Focus()
+            veli_ad_txt.Text = ""
+
+        ElseIf IsNumeric(veli_soyad_txt.Text) = True Then
+            MsgBox("Lütfen Veli Ad Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            veli_soyad_txt.Focus()
+            veli_soyad_txt.Text = ""
+
+        ElseIf IsNumeric(yakinlik_txt.Text) = True And yakinlik_txt.Text <> "" Then
+            MsgBox("Lütfen Yakınlığı Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            yakinlik_txt.Focus()
+            yakinlik_txt.Text = ""
+
+        ElseIf IsNumeric(veli_meslek_txt.Text) = True And veli_meslek_txt.Text <> "" Then
+            MsgBox("Lütfen Meslek Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            veli_meslek_txt.Focus()
+            veli_meslek_txt.Text = ""
+
+        ElseIf IsNumeric(veli_geliri_txt.Text) = False And veli_geliri_txt.Text <> "" Then
+            MsgBox("Lütfen Gelir Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            veli_geliri_txt.Focus()
+            veli_geliri_txt.Text = ""
+
+        ElseIf IsNumeric(veli_ilce_txt.Text) = True And veli_ilce_txt.Text <> "" Then
+            MsgBox("Lütfen İlçe Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            veli_ilce_txt.Focus()
+            veli_ilce_txt.Text = ""
+
+        ElseIf IsNumeric(veli_il_txt.Text) = True And veli_il_txt.Text <> "" Then
+            MsgBox("Lütfen İl Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            veli_il_txt.Focus()
+            veli_il_txt.Text = ""
+
+        ElseIf IsNumeric(veli_posta_kodu_txt.Text) = False And veli_posta_kodu_txt.Text <> "" Then
+            MsgBox("Lütfen Ev_Posta Kodu Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            veli_posta_kodu_txt.Focus()
+            veli_posta_kodu_txt.Text = ""
+
+        ElseIf IsNumeric(veli_ev_tel_txt.Text) = False And veli_ev_tel_txt.Text <> "" Then
+            MsgBox("Lütfen Veli Ev Telefonu Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            veli_ev_tel_txt.Focus()
+            veli_ev_tel_txt.Text = ""
+
+        ElseIf IsNumeric(veli_cep_txt.Text) = False And veli_cep_txt.Text <> "" Then
+            MsgBox("Lütfen Veli Cep Telefonu Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            veli_cep_txt.Focus()
+            veli_cep_txt.Text = ""
+
+        ElseIf IsNumeric(veli_is_ilce_txt.Text) = True And veli_is_ilce_txt.Text <> "" Then
+            MsgBox("Lütfen Veli'nin İş İlçe Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            veli_is_ilce_txt.Focus()
+            veli_is_ilce_txt.Text = ""
+
+        ElseIf IsNumeric(veli_is_ili_txt.Text) = True And veli_is_ili_txt.Text <> "" Then
+            MsgBox("Lütfen Veli'nin İş İl Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            veli_is_ili_txt.Focus()
+            veli_is_ili_txt.Text = ""
+
+        ElseIf IsNumeric(veli_is_tel_txt.Text) = False And veli_is_tel_txt.Text <> "" Then
+            MsgBox("Lütfen Veli'nin İş Telefon Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            veli_is_tel_txt.Focus()
+            veli_is_tel_txt.Text = ""
+
+        ElseIf IsNumeric(veli_is_fax_txt.Text) = False And veli_is_fax_txt.Text <> "" Then
+            MsgBox("Lütfen Veli'nin İş Fax Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            veli_is_fax_txt.Focus()
+            veli_is_fax_txt.Text = ""
+
+        ElseIf IsNumeric(veli_is_posta_kodu_txt.Text) = False And veli_is_posta_kodu_txt.Text <> "" Then
+            MsgBox("Lütfen Veli'nin İş posta Koduna Alanına Geçerli Değer Giriniz!!!", MsgBoxStyle.Exclamation)
+            veli_is_posta_kodu_txt.Focus()
+            veli_is_posta_kodu_txt.Text = ""
+
+        Else
+            veli.kontrol()
+        End If
+    End Sub
+
+    Private Sub veli_sil_btn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles veli_sil_btn.Click
+        veli.v_no = tc_txt.Text
+        veli.sil()
+    End Sub
+
+    Private Sub veli_guncelle_btn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles veli_guncelle_btn.Click
+        v_bilgi_ata()
+        veli.guncelle()
+    End Sub
+
+    Private Sub veli_temizle_btn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles veli_temizle_btn.Click
+        veli.temizle()
+    End Sub
+
+    Private Sub veli_cikis_btn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles veli_cikis_btn.Click
+        veli.cikis()
+    End Sub
+
+    Private Sub veli_ara_btn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles veli_ara_btn.Click
+        v_bilgi_ata()
+        veli.ara()
+    End Sub
+
+    Private Sub veli_listele_btn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles veli_listele_btn.Click
+        veli.listele()
+    End Sub
+
