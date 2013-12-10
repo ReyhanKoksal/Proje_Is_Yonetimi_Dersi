a12bff379efb624cc4eb067d9365d912d002d427
Reyhan Köksal <r3yh4n@hotmail.com>
2013-12-10 02:30:41 +0200

    Update devamsızlık.vb

--- devamsızlık.vb
+++ devamsızlık.vb
@@ -3,4 +3,9 @@ Public Class Devamsizlik
     Private dev_turu As String
     Private g As String
     Private o_no As Integer
+    
+    Public ad As String
+    Public derskodu As Integer
+    Public no As integer
+    Public soyad As String
   
