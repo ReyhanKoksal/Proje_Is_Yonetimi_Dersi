215b5babf55bc80938a1f14bc1103c2a009ef158
Reyhan Köksal <r3yh4n@hotmail.com>
2013-12-10 04:46:40 +0200

    AnaForm

--- Ogrenci_form.vb
+++ Ogrenci_form.vb
@@ -1,4 +1,14 @@
 Public Class ogrenci_frm
     Dim ogrenci As New ogrenci_bilgi
     Dim veli As New veli_bilgileri
-  
+    Public et As Integer
+    Public nokta As Integer
+    Public et1 As Integer
+    Public nokta1 As Integer
+    Dim dp As New prog
+    Dim sp As New senet
+    Dim devam As New Devamsizlik
+    Dim op As New odemeler
+    Public senetno As Integer
+
+
