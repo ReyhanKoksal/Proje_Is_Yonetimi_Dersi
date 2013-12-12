3e308419801439c6eb0d5b66b1f8b9c20618b25d
Reyhan Köksal <r3yh4n@hotmail.com>
2013-12-12 19:48:30 +0200

    Create prog.vb

---
+++ prog.vb
@@ -0,0 +1,8 @@
+Public Class prog
+    Private g As String
+    Private gun As String
+    Public prog_o_no As Integer
+    Public kod As String
+    Public ad As String
+    Public derskodu As Integer
+    Public soyad As String
